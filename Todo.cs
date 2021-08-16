1aa49461a90ca9b4950026a032a07300a7f10383
Michał ° <nukinash@interia.pl>
2021-08-16 22:35:34 +0200

    Dodaj pliki projektów.

---
+++ Todo.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * 1. Załadowanie pliku
+ * 2. Interfejs
+ * 3. Pobranie danych o pliku (długość, nazwa, rodzaj itp.)
+ * 4. Odtwarzanie/pauzowanie/przewijanie
+ * 5. Playlista
+ * 6. Głośność
+ * 7. Drag and drop
+ * 8. Odtwarzanie losowe
+ * 9. Wyszukiwarka
+ * 10. Załatwienie grafik u Chetoosa za boosting
+ * 11. Obrazek
+ * 12. Ulubione
+ * 
+ */
